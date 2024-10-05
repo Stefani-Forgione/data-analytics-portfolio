427f2a5ddac8d10f2a367e0e1ac2764b7634a178
Stefani-Forgione <145605635+Stefani-Forgione@users.noreply.github.com>
2024-10-05 18:26:45 -0400

    main

---
+++ config.py
@@ -0,0 +1,9 @@
+{
+  "db_name": "your_db",
+  "db_port": 3306,
+  "db_user": "your_username",
+  "db_password": "your_password",
+  "db_host": "localhost",
+  "required_packages": ["pandas", "sqlalchemy", "numpy", "scipy", "sklearn"]
+}
+
